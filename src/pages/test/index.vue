c4dd5de9fdeec57efe00cab7110c0e583309cc1e
Amanam Israel <amanam42@hotmail.com>
2021-04-06 23:23:22 +0100

    implemented timer

--- src/pages/test/index.vue
+++ src/pages/test/index.vue
@@ -1,12 +1,90 @@
 <template>
-  <div>test works</div>
+  <main>
+    <div class="container-fluid">
+      <div
+        class="wrapper d-flex flex-column justify-content-center align-items-center"
+      >
+        <div>
+          <p class="fs-5 text-right">{{ timer }}s remaining</p>
+          <div class="card">
+            <div class="card-body">
+              <form @submit.prevent="answered">
+                <p class="fs-3 text-center mb-3">
+                  The Stadium of Light is home to which English football club?
+                </p>
+                <div class="container-fluid">
+                  <div class="option mb-3">
+                    <input
+                      class="form-check-input"
+                      type="radio"
+                      name="option"
+                      id="option1"
+                    />
+                    <label class="ms-3 form-check-label" for="option1">
+                      West Bromwich Albion
+                    </label>
+                  </div>
+                  <button type="submit" class="p-3 w-100 btn btn-primary">
+                    Next question
+                  </button>
+                </div>
+              </form>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </main>
 </template>
 
 <script>
+import data from "@/assets/static/data/index";
+import store from "@/store/index";
 export default {
   name: "test",
+  data() {
+    return {
+      allQuestions: data().data,
+      answeredQuestions: {},
+      timer: 120,
+    };
+  },
+  computed: {},
+  methods: {
+    saveAnsweredQuestions() {
+      store.commit("setAnsweredQuestions", this.answeredQuestions);
+    },
+    answered() {
+      console.log("answered");
+    },
+  },
+  created() {
+    console.log(this.allQuestions);
+  },
+  mounted() {
+    const setTimer = setInterval(() => {
+      if (this.timer === 0) {
+        console.log("finished");
+        clearInterval(setTimer);
+        return;
+      }
+      this.timer -= 1;
+    }, 1000);
+  },
 };
 </script>
 
-<style scoped lang="scss">
+<style scoped >
+.wrapper {
+  height: 100vh;
+}
+div.card {
+  max-width: 600px;
+  width: auto;
+}
+.option {
+  background-color: aliceblue;
+  padding: 2em;
+  border-radius: 5px;
+}
 </style>
--- src/store/index.js
+++ src/store/index.js
@@ -7,11 +7,15 @@ export default new Vuex.Store({
   state: {
     userHasEnteredInfo: false,
     userInfo: {},
+    answeredQuestions: {},
   },
   mutations: {
     setUserEnteredInfoState(state, userInfo) {
       state.userInfo = userInfo;
       state.userHasEnteredInfo = true;
+    },
+    setAnsweredQuestions(state, questions) {
+      state.answeredQuestions = questions;
     },
   },
 });
